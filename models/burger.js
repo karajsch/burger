767e7e8c029bcb4b57ba3bb5db8df9513b747b16
Kara Schmidt <kara.jane.schmidt@gmail.com>
2021-01-12 15:53:56 -0500

    basic set up for styling

---
+++ models/burger.js
@@ -0,0 +1,8 @@
+var orm = require("../config/orm.js");
+
+var burger = {
+        selectAll: function (cb) {
+            orm.selectAll("burgers", function (res) {
+                cb(res);
+            })
+        }
