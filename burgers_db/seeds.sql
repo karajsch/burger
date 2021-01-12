62c9c670628b9b89e7d09d5ecd2253abb2551c27
Kara Schmidt <kara.jane.schmidt@gmail.com>
2021-01-12 16:03:22 -0500

    adding basic schema + seed sql

---
+++ burgers_db/seeds.sql
@@ -0,0 +1,2 @@
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ("tempeh burger with special sauce", false), ("impossible burger", true), ("smoked seitan burger", false), ("beet burger", true);
